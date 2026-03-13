2f4eeaae5c36d2cdc9e9b0182fd2bc16c68bca56
Kalle Ilves <kalleilv@gmail.com>
2026-03-13 14:07:26 +0200

    improve export script

--- slidev/export.mjs
+++ slidev/export.mjs
@@ -1,11 +1,21 @@
 #!/usr/bin/env zx
 
-import { usePowerShell, $, path, minimist } from "zx";
+import { usePowerShell, $, path, minimist, spinner } from "zx";
 
 usePowerShell();
 
 const argv = minimist(process.argv.slice(2), {});
 const filePath = argv._[1];
+
+if (!filePath) {
+  const changedSlides = await getChangedSlides();
+
+  for (const name of changedSlides) {
+    await exportFile(name);
+  }
+
+  process.exit(0);
+}
 
 const pathParts = filePath
   .split(path.sep)
@@ -13,8 +23,23 @@ const pathParts = filePath
 
 const fileName = pathParts[0].split(".")[0];
 
-await $`npx slidev export ${fileName}.md --timeout 60000 --output ${path.join(
-  "..",
-  "slides",
-  `${fileName}.pdf`
-)}`;
+await exportFile(fileName);
+
+async function getChangedSlides() {
+  const status = await $`git status --short`;
+  const paths = status
+    .toString()
+    .split("\n")
+    .filter((path) => path.endsWith(".md"))
+    .map((path) => path.split(" ").at(-1).replace(/\.md/, ""));
+
+  return paths;
+}
+
+async function exportFile(name) {
+  await spinner(`Exporting ${name} to PDF...`, () => $`npx slidev export ${name}.md --timeout 60000 --output ${path.join(
+    "..",
+    "slides",
+    `${name}.pdf`,
+  )}`)
+}
