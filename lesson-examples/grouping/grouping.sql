c0a1cffd5d28cb8afa2933dbdc31acbebf49725f
Kalle Ilves <kalleilv@gmail.com>
2026-09-13 08:28:02 +0300

    improvements

--- lesson-examples/grouping/grouping.sql
+++ lesson-examples/grouping/grouping.sql
@@ -1,3 +1,8 @@
 -- What's the average grade from each course? Display course code and average grade
 
 -- Which campuses have more than 1 teacher? Display campus code and number of teachers
+
+-- Which course implementation has the most participants? 
+-- Display course code, instance number and number of parcitipants
+
+-- Which teachers teach the course "a450"? Display teacher number first name and surname
